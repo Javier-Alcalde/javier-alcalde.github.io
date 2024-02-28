5d68f45d2e1f58b03d6f043e68791f1c6eeca93f
AideGilIrigoyen <agilirigoi@educacion.navarra.es>
2024-02-28 17:39:37 +0100

    index

--- html/A12_JS/js/main.js
+++
@@ -1,8 +0,0 @@
-function mostrarMenuMobil() {
-    var menu = document.getElementById("menu")
-    if (menu.className == "menu") {
-        menu.className = "menu_mostrado"
-    } else {
-        menu.className = "menu"
-    }
-}
